cf72c300474c9ceb207c5bf947d78a3ed7f63ea9
Keith0309 <keithbanaybanay@gmail.com>
2023-07-01 00:10:08 +0800

    removed footer green for signin and signup

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -1,102 +1,105 @@
 import DefaultLayout from "../../components/Layout/DefaultLayout/DefaultLayout";
 import React, { useState } from "react";
-import "./SignIn.css"
-import nchm_logo from "../../assets/images/ncmh_logo.png"
+import "./SignIn.css";
+import nchm_logo from "../../assets/images/ncmh_logo.png";
 import { Link } from "react-router-dom";
 import Footer2 from "../../components/Footer2/Footer2";
 
-
-
 const SignIn = () => {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-const [errorMessages, setErrorMessages] = useState({});
-const [isSubmitted, setIsSubmitted] = useState(false);
+  const database = [
+    {
+      username: "user1",
+      password: "pass1",
+    },
+    {
+      username: "user2",
+      password: "pass2",
+    },
+  ];
 
-const database = [
-  {
-    username: "user1",
-    password: "pass1"
-  },
-  {
-    username: "user2",
-    password: "pass2"
-  }
-];
+  const errors = {
+    uname: "You have entered an invalid email. Please try again.",
+    pass: "Your email and password does not match. Please try again",
+  };
 
-const errors = {
-  uname: "You have entered an invalid email. Please try again.",
-  pass: "Your email and password does not match. Please try again"
-};
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
 
-const handleSubmit = (event) => {
-  //Prevent page reload
-  event.preventDefault();
+    var { uname, pass } = document.forms[0];
 
-  var { uname, pass } = document.forms[0];
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
 
-  // Find user login info
-  const userData = database.find((user) => user.username === uname.value);
-
-  // Compare user info
-  if (userData) {
-    if (userData.password !== pass.value) {
-      // Invalid password
-      setErrorMessages({ name: "pass", message: errors.pass });
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
     } else {
-      setIsSubmitted(true);
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
     }
-  } else {
-    // Username not found
-    setErrorMessages({ name: "uname", message: errors.uname });
-  }
-};
+  };
 
-// Generate JSX code for error message
-const renderErrorMessage = (name) =>
-  name === errorMessages.name && (
-    <div className="error">{errorMessages.message}</div>
-  );
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
 
-// JSX code for login form
-const renderForm = (
-  <div className="form">
-    <form onSubmit={handleSubmit}>
-      <div className="input-container">
-        <label className="fw-bold">Email Address </label>
-        <input placeholder=" email@youremail.com" type="text" name="uname" required />
-        {renderErrorMessage("uname")}
-      </div>
-      <div className="input-container">
-        <label className="fw-bold">Password </label>
-        <input placeholder=" password" type="password" name="pass" required />
-        {renderErrorMessage("pass")}
-      </div>
-      <div className="button-container">
-        <button className="login-button fw-bold" type="submit">LOGIN</button>
-      </div>
-    </form>
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label className="fw-bold">Email Address </label>
+          <input
+            placeholder=" email@youremail.com"
+            type="text"
+            name="uname"
+            required
+          />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label className="fw-bold">Password </label>
+          <input placeholder=" password" type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <button className="login-button fw-bold" type="submit">
+            LOGIN
+          </button>
+        </div>
+      </form>
 
-    <div className="text_link">
-      <p>Dont have an account yet? &nbsp;
-      <Link className="signup_link fw-bold"to="/signup">
-         Create one!
-        </Link>
-      </p>
-      <p>
-      <Link className="forgot_pass fw-bold"to="/forgot_pass">
-         Forgot password?
-        </Link>
-      </p>
+      <div className="text_link">
+        <p>
+          Dont have an account yet? &nbsp;
+          <Link className="signup_link fw-bold" to="/signup">
+            Create one!
+          </Link>
+        </p>
+        <p>
+          <Link className="forgot_pass fw-bold" to="/forgot_pass">
+            Forgot password?
+          </Link>
+        </p>
+      </div>
     </div>
-  </div>
-);
-
+  );
 
   return (
     <DefaultLayout>
-
-    {/* Home > Sign in */}
-    <div className="container p-0">
+      {/* Home > Sign in */}
+      <div className="container p-0">
         <div className="d-flex directory-div mt-2 py-1 ">
           <p className="directory-text d-flex ms-4 mt-2 fw-bold">
             <Link className="directory_link " to="/">
@@ -112,17 +115,22 @@ const renderForm = (
           <h1>SignIn PAge</h1>
         </div> */}
 
-          <div className="signin-form mx-auto">
-            <div className="login-form mx-auto" >
-              <div className="nchm-container text-center" ><img className="ncmh-logo" src={nchm_logo} alt="ncmh_logo"></img></div>
-              <div className="signin-text text-center fw-bolder my-3">Sign in to your account</div>
-              {isSubmitted ? <div className="text-center">User is successfully logged in!</div> : renderForm}
+        <div className="signin-form mx-auto">
+          <div className="login-form mx-auto">
+            <div className="nchm-container text-center">
+              <img className="ncmh-logo" src={nchm_logo} alt="ncmh_logo"></img>
             </div>
+            <div className="signin-text text-center fw-bolder my-3">
+              Sign in to your account
+            </div>
+            {isSubmitted ? (
+              <div className="text-center">User is successfully logged in!</div>
+            ) : (
+              renderForm
+            )}
           </div>
-
+        </div>
       </div>
-
-      <Footer2 />
     </DefaultLayout>
   );
 };
--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -1,102 +1,114 @@
 import DefaultLayout from "../../components/Layout/DefaultLayout/DefaultLayout";
 import React, { useState } from "react";
-import "./SignUp.css"
-import nchm_logo from "../../assets/images/ncmh_logo.png"
+import "./SignUp.css";
+import nchm_logo from "../../assets/images/ncmh_logo.png";
 import { Link } from "react-router-dom";
 import Footer2 from "../../components/Footer2/Footer2";
 
-
-
 const SignUp = () => {
-
   const [errorMessages, setErrorMessages] = useState({});
   const [isAccountCreated, setisAccountCreated] = useState(false);
 
-const errors = {
-  email: "You have entered an invalid email. Please try again.",
-  pass: "The password you entered is incorrect. Please try again",
-  passnotmatch: "Your password does not match. Please try again."
-};
+  const errors = {
+    email: "You have entered an invalid email. Please try again.",
+    pass: "The password you entered is incorrect. Please try again",
+    passnotmatch: "Your password does not match. Please try again.",
+  };
 
-const handleSubmit = (event) => {
-  //Prevent page reload
-  event.preventDefault();
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
 
-  var { email, pass, passmatch } = document.forms[0];
+    var { email, pass, passmatch } = document.forms[0];
 
-  // Find user login info
-  // const userData = database.find((user) => user.username === uname.value);
+    // Find user login info
+    // const userData = database.find((user) => user.username === uname.value);
 
-  // var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+    // var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
 
-  // Compare user info
-  if (email.value.includes("@" && ".com")) {
-    alert(email.value);
-    if (pass.value !== passmatch.value) {
-      // Invalid password
-      setErrorMessages({ name: "passnotmatch", message: errors.passnotmatch });
+    // Compare user info
+    if (email.value.includes("@" && ".com")) {
+      alert(email.value);
+      if (pass.value !== passmatch.value) {
+        // Invalid password
+        setErrorMessages({
+          name: "passnotmatch",
+          message: errors.passnotmatch,
+        });
+      } else {
+        setisAccountCreated(true);
+      }
     } else {
-      setisAccountCreated(true);
+      // Username not found
+      setErrorMessages({ name: "email", message: errors.email });
     }
-  } else {
-    // Username not found
-    setErrorMessages({ name: "email", message: errors.email });
-  }
-};
+  };
 
-// Generate JSX code for error message
-const renderErrorMessage = (name) =>
-  name === errorMessages.name && (
-    <div className="error">{errorMessages.message}</div>
-  );
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
 
-// JSX code for login form
-const renderForm = (
-  <div className="form">
-    <form onSubmit={handleSubmit}>
-      <div className="input-container">
-        <label className="fw-bold">Email Address </label>
-        <input placeholder=" email@youremail.com" type="text" name="email" required />
-        {renderErrorMessage("email")}
-      </div>
-      <div className="input-container">
-        <label className="fw-bold">First Name </label>
-        <input placeholder=" First Name" type="text" name="fName" required />
-      </div>
-      <div className="input-container">
-        <label className="fw-bold">Last Name </label>
-        <input placeholder=" Last Name" type="text" name="lName" required />
-      </div>
-      <div className="input-container">
-        <label className="fw-bold">Password </label>
-        <input placeholder=" password" type="password" name="pass" required />
-        {renderErrorMessage("pass")}
-      </div>
-      <div className="input-container">
-        <label className="fw-bold">Re-type your Password </label>
-        <input placeholder=" password" type="password" name="passmatch" required />
-        {renderErrorMessage("passnotmatch")}
-      </div>
-      <div className="button-container">
-        <button className="login-button fw-bold" type="submit">Create Account</button>
-      </div>
-    </form>
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label className="fw-bold">Email Address </label>
+          <input
+            placeholder=" email@youremail.com"
+            type="text"
+            name="email"
+            required
+          />
+          {renderErrorMessage("email")}
+        </div>
+        <div className="input-container">
+          <label className="fw-bold">First Name </label>
+          <input placeholder=" First Name" type="text" name="fName" required />
+        </div>
+        <div className="input-container">
+          <label className="fw-bold">Last Name </label>
+          <input placeholder=" Last Name" type="text" name="lName" required />
+        </div>
+        <div className="input-container">
+          <label className="fw-bold">Password </label>
+          <input placeholder=" password" type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="input-container">
+          <label className="fw-bold">Re-type your Password </label>
+          <input
+            placeholder=" password"
+            type="password"
+            name="passmatch"
+            required
+          />
+          {renderErrorMessage("passnotmatch")}
+        </div>
+        <div className="button-container">
+          <button className="login-button fw-bold" type="submit">
+            Create Account
+          </button>
+        </div>
+      </form>
 
-    <div className="text_link">
-      <p>Already have an account? &nbsp;
-      <Link className="signin_link fw-bold"to="/signin">
-         Sign In
-        </Link>
-      </p>
+      <div className="text_link">
+        <p>
+          Already have an account? &nbsp;
+          <Link className="signin_link fw-bold" to="/signin">
+            Sign In
+          </Link>
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
 
   return (
     <DefaultLayout>
-
-    {/* Home > Create Account */}
-    <div className="container p-0">
+      {/* Home > Create Account */}
+      <div className="container p-0">
         <div className="d-flex directory-div mt-2 py-1 ">
           <p className="directory-text d-flex ms-4 mt-2 fw-bold">
             <Link className="directory_link " to="/">
@@ -113,15 +125,23 @@ const renderForm = (
         </div> */}
 
         <div className="signup-form mx-auto">
-            <div className="login-form mx-auto" >
-              <div className="nchm-container text-center" ><img className="ncmh-logo" src={nchm_logo} alt="ncmh_logo"></img></div>
-              <div className="signup-text text-center fw-bolder my-3">Create your account</div>
-              {isAccountCreated ? <div className="text-center">You have successfully created an account!</div> : renderForm}
+          <div className="login-form mx-auto">
+            <div className="nchm-container text-center">
+              <img className="ncmh-logo" src={nchm_logo} alt="ncmh_logo"></img>
             </div>
+            <div className="signup-text text-center fw-bolder my-3">
+              Create your account
+            </div>
+            {isAccountCreated ? (
+              <div className="text-center">
+                You have successfully created an account!
+              </div>
+            ) : (
+              renderForm
+            )}
           </div>
+        </div>
       </div>
-
-      <Footer2 />
     </DefaultLayout>
   );
 };
