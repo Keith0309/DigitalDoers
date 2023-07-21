49c9a2b50b48793a19c7e47df745f115bcc223a1
David Mendoza <davidmndza98@gmail.com>
2023-07-22 01:26:53 +0800

    fixed best seller items on homepage

--- src/assets/best_sellers/best_seller_data.js
+++ src/assets/best_sellers/best_seller_data.js
@@ -4,7 +4,6 @@ const best_sellers = [
         product_name: "Bondi Sands Sunscreen Sport SPF 50+",
         description: "Nourishes and shields the skin from sun exposure, through a fragrance-free and gentle formula that is suitable for sensitive skin.",
         price: 387.39,
-        currency: "₱",
         image: "./best_products/bondi_sands_suncreen.webp",
     },
     {
@@ -12,7 +11,6 @@ const best_sellers = [
         product_name: "Carex Original Antibacterial Hand Wash ",
         description: "Protects the skin’s natural antibacterial defences 2 hours after washing. Always practice good hand hygiene.",
         price: 203.68,
-        currency: "₱",
         image: "./best_products/carex_hand_soap.webp",
     },
     {
@@ -20,17 +18,8 @@ const best_sellers = [
         product_name: "PURELL Advanced Hand Sanitizer 59ml",
         description: "Stay clean without soap or water! Kills most common, illness-causing germs.",
         price: 100.00,
-        currency: "₱",
         image: "./best_products/purell_hand_sanitizer.jpg",
     },
-    // {
-    //     id: 4,
-    //     product_name: "Tide PODS Laundry Detergent Liquid Pacs 3-in-1 Original",
-    //     description: "Enjoy the super-concentrated detergent, odor fighters, and stain removers in each capsule to clean, freshen, and rejuvenate clothes.",
-    //     price: 31.60,
-    //     currency: "₱",
-    //     image: "./best_products/tide_pods_laundry.jpg",
-    // },
 ]
 
 export default best_sellers;
